919592ce62350002f6e7e402e41f11076a17cf54
park-minsun <ioioios00@naver.com>
2022-11-04 00:23:44 +0900

    select box start

---
+++ script/main.js
@@ -0,0 +1,11 @@
+
+      var swiper = new Swiper(".mainVisual > .mySwiper", {
+        pagination: {
+          el: ".swiper-pagination",
+          type: "fraction",
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
